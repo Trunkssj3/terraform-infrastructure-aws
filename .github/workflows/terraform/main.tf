71bce536500aa56f05d48531f282d963541b06cf
Trunkssj3 <133145654+Trunkssj3@users.noreply.github.com>
2025-07-23 13:37:04 +0300

    fasdf

--- .github/workflows/terraform/main.tf
+++ .github/workflows/terraform/main.tf
@@ -1,7 +1,9 @@
 
+
 provider "aws" {
   region = "ap-south-1"
 }
+
 
 resource "aws_security_group" "allow_http_ssh" {
   name        = "allow_http_ssh"
@@ -32,26 +34,28 @@ resource "aws_security_group" "allow_http_ssh" {
 }
 
 
+
 resource "aws_instance" "flask_docker" {
-  ami           = "ami-03c156cf4a6b389e6" # Ubuntu 22.04 LTS Mumbai
-  instance_type = "t2.micro"
+  ami                    = "ami-03c156cf4a6b389e6" # Ubuntu 22.04 LTS Mumbai
+  instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_http_ssh.id]
 
   user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y docker.io
-              systemctl start docker
-              systemctl enable docker
+#!/bin/bash
+apt-get update
+apt-get install -y docker.io
+systemctl start docker
+systemctl enable docker
 
-              docker run -d --name flask-app -p 5000:5000 tiangolo/uwsgi-nginx-flask:python3.8 \
-                bash -c "echo 'from flask import Flask; app = Flask(__name__); @app.route(\"/\")\\ndef home(): return \\\"Hello from Flask on EC2!\\\"; if __name__ == \\\"__main__\\\": app.run(host=\\\"0.0.0.0\\\")' > /app/main.py && python /app/main.py"
-            EOF
+docker run -d --name flask-app -p 5000:5000 tiangolo/uwsgi-nginx-flask:python3.8 \
+  bash -c "echo 'from flask import Flask; app = Flask(__name__); @app.route(\"/\")\\ndef home(): return \\\"Hello from Flask on EC2!\\\"; if __name__ == \\\"__main__\\\": app.run(host=\\\"0.0.0.0\\\")' > /app/main.py && python /app/main.py"
+EOF
 
   tags = {
     Name = "FlaskDockerEC2"
   }
 }
+
 
 output "ec2_public_ip" {
   value       = aws_instance.flask_docker.public_ip
